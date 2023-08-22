3b1f6dad2806e1238a59b76cba51645eab4fad2e
Giovani Dalmas <giovanidalmas053@gmail.com>
2023-08-22 07:56:54 -0300

    api todas as postagens

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -22,3 +22,12 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollPos = currentTop;
     });
 })
+
+
+angular.module("blog", []);
+
+angular.module("blog").controller("Rest", function ($scope, $http) {
+  $http.get("https://api-rest-post-diegocandido.herokuapp.com/postagens").success(function (data) {
+      $scope.publicacoes = data;
+    });
+});
